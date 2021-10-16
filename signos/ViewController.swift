bebc95ec206cdff3974b789a472c74d04ff8de2a
Eric Ertl <eric@ertl.com.ar>
2021-10-16 12:15:35 -0300

    Initial Commit

---
+++ signos/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  signos
+//
+//  Created by Eric Ertl on 16/10/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
